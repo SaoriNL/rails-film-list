10d4f862f0e58e791192ce07e8cbcc0c9fe2368c
Saori Mita <saorimita4@gmail.com>
2022-08-20 16:35:34 +0200

    development

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,25 @@
+class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark.delete
+  end
+
+  private
+  def bookmark_params
+    params.require(:bookmark).permit(:comment)
+  end
+end
