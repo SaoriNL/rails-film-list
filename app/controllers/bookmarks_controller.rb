645e6c52a398b295c0fcce9461dd0b0dcb381566
Saori Mita <saorimita4@gmail.com>
2022-08-20 18:24:32 +0200

    backend done thanks to stewan

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,12 +1,14 @@
 class BookmarksController < ApplicationController
+  before_action :set_list, only: %i[new create]
+
   def new
     @bookmark = Bookmark.new
-    @list = List.find(params[:list_id])
   end
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
+    @bookmark.movie = Movie.find(bookmark_params[:movie_id])
     if @bookmark.save
       redirect_to list_path(@list)
     else
@@ -15,11 +17,18 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
+    @bookmark = Bookmark.find(params[:id])
     @bookmark.delete
+    redirect_to list_path(@bookmark[:list_id])
   end
 
   private
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
   def bookmark_params
-    params.require(:bookmark).permit(:comment)
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
   end
 end
